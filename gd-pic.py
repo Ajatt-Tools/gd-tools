b11fc730300435c1a59c87889924ac73c3a63273
GenjiFujimoto <dratschuk.d@gmail.com>
2023-03-12 21:44:09 +0100

    Add python alternative for gd-pics

--- gd-pic.py
+++ gd-pic.py
@@ -6,7 +6,7 @@ import sys
 
 search_term = sys.argv[1]
 
-response = requests.get(f"https://www.bing.com/images/search?q={search_term}&first=1")
+response = requests.get(f"https://search.brave.com/images?q={search_term}")
 soup = BeautifulSoup(response.text, "html.parser")
 image_tags = soup.find_all("img", class_="mimg")
 
@@ -16,6 +16,6 @@ for img in image_tags[:5]:
     if src:
         image_urls.append(f'<img src="{src}">')
 
-html_output = f'<div id="golden-images"><section id="image-grid" class="japanese_gothic focus" lang="ja"><div class="image-list"> {" ".join(image_urls)} </div></section></div>'
+html_output = f'<div id="golden-images"><section id="image-grid"><div class="image-list"> {" ".join(image_urls)} </div></section></div>'
 
 print(html_output)
