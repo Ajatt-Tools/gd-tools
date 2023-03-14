ecda1aeae82ad25a7bc34faf3174f08269a0d758
GenjiFujimoto <dratschuk.d@gmail.com>
2023-03-14 12:56:16 +0100

    Add bash implementation of gd-pics

--- gd-pics/gd-pic.py
+++ gd-pics/gd-pic.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-
 import requests
 from bs4 import BeautifulSoup
 import sys
 
 search_term = sys.argv[1]
-response = requests.get(f"https://www.bing.com/images/search?q={search_term}")
+response = requests.get(f" https://www.bing.com/images/search?q={search_term}")
 soup = BeautifulSoup(response.text, "html.parser")
 image_tags = soup.find_all("img", class_="mimg")
-# print(soup)
 
 image_urls = []
 for img in image_tags[:5]:
