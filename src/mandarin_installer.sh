d995501941356861c61f4283258e3f076fea6f18
Konstantin <alexandroviski8299@gmail.com>
2023-08-30 13:29:05 -0400

    Update mandarin_installer.sh

--- src/mandarin_installer.sh
+++ src/mandarin_installer.sh
@@ -10,7 +10,7 @@ fi
 
 zip_file="res/gd-mandarin_dict/gd-mandarin.zip"
 
-unzip "$zip_file" -d "$target_dir"
+unzip  -j "$zip_file" -d "$target_dir"
 
 # Move the files to their specific locations
 mv "$target_dir"char.bin "$target_dir"dicrc "$target_dir"matrix.bin "$target_dir"sys.dic "$target_dir"unk.dic "$target_dir"user.dic "$target_dir"
